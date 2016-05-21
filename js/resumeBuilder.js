84a5e299db50e8f61373d186b722abe5ad604e7e
v0lmag <vzerolmag@gmail.com>
2016-05-21 20:02:58 +0300

    Quiz: Save and .append() Data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var awesomeThoughts;
+ awesomeThoughts = "I am Max Volotsky and I am AWESOME!";
+ console.log(awesomeThoughts);
